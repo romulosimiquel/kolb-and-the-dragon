f56e7667a0316946627b454f7b0c381dcd551787
Rômulo Simiquel <romulosimiquel@gmail.com>
2021-03-17 13:21:05 -0300

    inicia o controller de escolhas

--- src/controllers/choice.ts
+++ src/controllers/choice.ts
@@ -1,6 +1,11 @@
-import { Controller } from '@overnightjs/core';
+import { Controller, Get } from '@overnightjs/core';
+import { Request, Response } from 'express';
 
 @Controller('choice')
 export class ChoiceController {
-    
+
+    @Get('')
+    public getChoiceResult(_: Request, res: Response) {
+
+    }
 }
